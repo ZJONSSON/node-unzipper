71493c5ed225e2fa199bf329e9fcdad353935bd4
Ayush Aher <ayushaher118@gmail.com>
2024-06-07 22:19:22 +0530

    Moved from fstream to fs-extra package, as fstream is no longer supported and its dependencies contains vulnerability
    Removed and replaced vulnerable packages

--- test/extractFromUrl.js
+++ test/extractFromUrl.js
@@ -1,25 +1,41 @@
 const test = require("tap").test;
 const fs = require("fs");
 const unzip = require("../");
-const os = require("os");
-const request = require("request");
+const axios = require("axios");
+const path = require("path");
 
-test("extract zip from url", function (t) {
-  const extractPath = os.tmpdir() + "/node-unzip-extract-fromURL"; // Not using path resolve, cause it should be resolved in extract() function
-  unzip.Open.url(
-    request,
-    "https://github.com/h5bp/html5-boilerplate/releases/download/v7.3.0/html5-boilerplate_v7.3.0.zip"
-  )
-    .then(function(d) { return d.extract({ path: extractPath }); })
-    .then(function() {
-      const dirFiles = fs.readdirSync(extractPath);
-      const isPassing =
-        dirFiles.length > 10 &&
-        dirFiles.indexOf("css") > -1 &&
-        dirFiles.indexOf("index.html") > -1 &&
-        dirFiles.indexOf("favicon.ico") > -1;
+test("extract zip from url", async function (t) {
+  const extractPath = path.join("../node-unzip-extract-fromURL"); // Ensure path is constructed correctly
+  const url =
+    "https://github.com/h5bp/html5-boilerplate/releases/download/v7.3.0/html5-boilerplate_v7.3.0.zip";
 
-      t.equal(isPassing, true);
-      t.end();
+  try {
+    // Fetch the zip file
+    const response = await axios({
+      method: "get",
+      url: url,
+      responseType: "arraybuffer", // Download the file as a buffer
     });
+
+    // Buffer the response data
+    const buffer = Buffer.from(response.data);
+
+    // Extract the buffer
+    const directory = await unzip.Open.buffer(buffer);
+    await directory.extract({ path: extractPath });
+
+    // Check extracted files
+    const dirFiles = fs.readdirSync(extractPath);
+    const isPassing =
+      dirFiles.length > 10 &&
+      dirFiles.indexOf("css") > -1 &&
+      dirFiles.indexOf("index.html") > -1 &&
+      dirFiles.indexOf("favicon.ico") > -1;
+
+    t.equal(isPassing, true);
+  } catch (error) {
+    t.fail(error.message);
+  } finally {
+    t.end();
+  }
 });
--- test/openUrl.js
+++ test/openUrl.js
@@ -2,10 +2,9 @@ const test = require('tap').test;
 const fs = require('fs');
 const path = require('path');
 const unzip = require('../');
-const request = require('request');
 
 test("get content of a single file entry out of a 502 MB zip from web", function (t) {
-  return unzip.Open.url(request, 'https://github.com/twbs/bootstrap/releases/download/v4.0.0/bootstrap-4.0.0-dist.zip')
+  return unzip.Open.url('https://github.com/twbs/bootstrap/releases/download/v4.0.0/bootstrap-4.0.0-dist.zip')
     .then(function(d) {
       const file = d.files.filter(function(d) {
         return d.path === 'css/bootstrap-reboot.min.css';
