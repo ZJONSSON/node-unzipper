78be07dcde727c264feae63800e44a294f5d1de5
Ziggy Jonsson <ziggy.jonsson.nyc@gmail.com>
2024-07-14 11:29:35 -0400

    refactor

--- test/office-files.js
+++ test/office-files.js
@@ -1,6 +1,7 @@
 const test = require('tap').test;
 const path = require('path');
 const unzip = require('../');
+const NoopStream = require('../lib/NoopStream');
 
 test("get content a docx file without errors", async function () {
   const archive = path.join(__dirname, '../testData/office/testfile.docx');
@@ -14,4 +15,18 @@ test("get content a xlsx file without errors", async function () {
 
   const directory = await unzip.Open.file(archive);
   await Promise.all(directory.files.map(file => file.buffer()));
+});
+
+test("stream retries when the local file header indicates bigger size than central directory", async function (t) {
+  const archive = path.join(__dirname, '../testData/office/testfile.xlsx');
+  let retries = 0, size = 0;
+  const directory = await unzip.Open.file(archive, {padding: 10});
+  const stream = directory.files[0].stream();
+  stream.on('streamRetry', _size => {
+    retries += 1;
+    size = _size;
+  });
+  await new Promise(resolve => stream.pipe(new NoopStream()).on('finish', resolve));
+  t.ok(retries === 1, 'retries once');
+  t.ok(size > 0, 'size is set');
 });
